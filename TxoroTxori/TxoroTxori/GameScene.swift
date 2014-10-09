97ac8ff45cc84415f6d98da700c3bb4c9c48069d
itermar <itermar@gmail.com>
2014-10-09 21:04:29 +0200

    Primer comió - Introducción de todos los assets y desarrollo de la primera versión del juego.

--- TxoroTxori/TxoroTxori/GameScene.swift
+++ TxoroTxori/TxoroTxori/GameScene.swift
@@ -7,39 +7,284 @@
 //
 
 import SpriteKit
+import AVFoundation
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
+    let verticalPipeGap = 200.0
+    
+    var bird:SKSpriteNode!
+    var skyColor:SKColor!
+    var pipeTextureUp:SKTexture!
+    var pipeTextureDown:SKTexture!
+    var movePipesAndRemove:SKAction!
+    var moving:SKNode!
+    var pipes:SKNode!
+    var canRestart = Bool()
+    var scoreLabelNode:SKLabelNode!
+    var score = NSInteger()
+    
+    let birdCategory: UInt32 = 1 << 0
+    let worldCategory: UInt32 = 1 << 1
+    let pipeCategory: UInt32 = 1 << 2
+    let scoreCategory: UInt32 = 1 << 3
+    
+    var audioPlayer = AVAudioPlayer()
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        canRestart = false
+        
+        // setup physics
+        self.physicsWorld.gravity = CGVectorMake( 0.0, -9.8 )
+        self.physicsWorld.contactDelegate = self
+        
+        // setup background color
+        skyColor = SKColor(red: 81.0/255.0, green: 192.0/255.0, blue: 201.0/255.0, alpha: 1.0)
+        self.backgroundColor = skyColor
+        
+        moving = SKNode()
+        self.addChild(moving)
+        pipes = SKNode()
+        moving.addChild(pipes)
+        
+        // ground
+        let groundTexture = SKTexture(imageNamed: "fondoScroll")
+        groundTexture.filteringMode = .Nearest // shorter form for SKTextureFilteringMode.Nearest
+        
+        let moveGroundSprite = SKAction.moveByX(-groundTexture.size().width * 2.0, y: 0, duration: NSTimeInterval(0.02 * groundTexture.size().width * 2.0))
+        let resetGroundSprite = SKAction.moveByX(groundTexture.size().width * 2.0, y: 0, duration: 0.0)
+        let moveGroundSpritesForever = SKAction.repeatActionForever(SKAction.sequence([moveGroundSprite,resetGroundSprite]))
+        
+        for var i:CGFloat = 0; i < 2.0 + self.frame.size.width / ( groundTexture.size().width * 2.0 ); ++i {
+            let sprite = SKSpriteNode(texture: groundTexture)
+            sprite.setScale(2.0)
+            sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2.0)
+            sprite.runAction(moveGroundSpritesForever)
+            moving.addChild(sprite)
+        }
+        
+        // skyline
+        let skyTexture = SKTexture(imageNamed: "background")
+        skyTexture.filteringMode = .Nearest
+        
+        let moveSkySprite = SKAction.moveByX(-skyTexture.size().width * 2.0, y: 0, duration: NSTimeInterval(0.1 * skyTexture.size().width * 2.0))
+        let resetSkySprite = SKAction.moveByX(skyTexture.size().width * 2.0, y: 0, duration: 0.0)
+        let moveSkySpritesForever = SKAction.repeatActionForever(SKAction.sequence([moveSkySprite,resetSkySprite]))
+        
+        for var i:CGFloat = 0; i < 2.0 + self.frame.size.width / ( skyTexture.size().width * 2.0 ); ++i {
+            let sprite = SKSpriteNode(texture: skyTexture)
+            sprite.setScale(1.0)
+            sprite.zPosition = -20
+            sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2.0 + groundTexture.size().height * 2.0)
+            sprite.runAction(moveSkySpritesForever)
+            moving.addChild(sprite)
+        }
+        
+        // create the pipes textures
+        pipeTextureUp = SKTexture(imageNamed: "chimenea")
+        pipeTextureUp.filteringMode = .Nearest
+        pipeTextureDown = SKTexture(imageNamed: "txistu")
+        pipeTextureDown.filteringMode = .Nearest
+        
+        // create the pipes movement actions
+        let distanceToMove = CGFloat(self.frame.size.width + 2.0 * pipeTextureUp.size().width)
+        let movePipes = SKAction.moveByX(-distanceToMove, y:0.0, duration:NSTimeInterval(0.01 * distanceToMove))
+        let removePipes = SKAction.removeFromParent()
+        movePipesAndRemove = SKAction.sequence([movePipes, removePipes])
+        
+        // spawn the pipes
+        let spawn = SKAction.runBlock({() in self.spawnPipes()})
+        let delay = SKAction.waitForDuration(NSTimeInterval(2.0))
+        let spawnThenDelay = SKAction.sequence([spawn, delay])
+        let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
+        self.runAction(spawnThenDelayForever)
+        
+        // setup our bird
+        let birdTexture1 = SKTexture(imageNamed: "txoriVuelo1")
+        birdTexture1.filteringMode = .Nearest
+        let birdTexture2 = SKTexture(imageNamed: "txoriVuelo2")
+        birdTexture2.filteringMode = .Nearest
+        
+        let anim = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.2)
+        let flap = SKAction.repeatActionForever(anim)
+        
+        bird = SKSpriteNode(texture: birdTexture1)
+        bird.setScale(1.0)
+        bird.position = CGPoint(x: self.frame.size.width * 0.35, y:self.frame.size.height * 0.6)
+        bird.runAction(flap)
+        
+        
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2.0)
+        bird.physicsBody?.dynamic = true
+        bird.physicsBody?.allowsRotation = false
+        
+        bird.physicsBody?.categoryBitMask = birdCategory
+        bird.physicsBody?.collisionBitMask = worldCategory | pipeCategory
+        bird.physicsBody?.contactTestBitMask = worldCategory | pipeCategory
+        
+        self.addChild(bird)
+        
+        // create the ground
+        var ground = SKNode()
+        ground.position = CGPointMake(0, groundTexture.size().height)
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, groundTexture.size().height * 2.0))
+        ground.physicsBody?.dynamic = false
+        ground.physicsBody?.categoryBitMask = worldCategory
+        self.addChild(ground)
+        
+        // Initialize label and create a label which holds the score
+        score = 0
+        scoreLabelNode = SKLabelNode(fontNamed:"Avenir")
+        scoreLabelNode.position = CGPointMake( CGRectGetMidX( self.frame ), 3 * self.frame.size.height / 4 )
+        scoreLabelNode.zPosition = 100
+        scoreLabelNode.text = String(score)
+        self.addChild(scoreLabelNode)
+        
+        
+        // Play music
+        self.startPintxo()
+        
+    }
+    
+    func spawnPipes() {
+        let pipePair = SKNode()
+        pipePair.position = CGPointMake( self.frame.size.width + pipeTextureUp.size().width * 2, 0 )
+        pipePair.zPosition = -10
+        
+        let height = UInt32( UInt(self.frame.size.height / 4) )
+        let y = arc4random() % height + height
+        
+        let pipeDown = SKSpriteNode(texture: pipeTextureDown)
+        pipeDown.setScale(1.0)
+        pipeDown.position = CGPointMake(0.0, CGFloat(Double(y)) + pipeDown.size.height + CGFloat(verticalPipeGap))
+        
+        
+        pipeDown.physicsBody = SKPhysicsBody(rectangleOfSize: pipeDown.size)
+        pipeDown.physicsBody?.dynamic = false
+        pipeDown.physicsBody?.categoryBitMask = pipeCategory
+        pipeDown.physicsBody?.contactTestBitMask = birdCategory
+        pipePair.addChild(pipeDown)
+        
+        let pipeUp = SKSpriteNode(texture: pipeTextureUp)
+        pipeUp.setScale(1.0)
+        pipeUp.position = CGPointMake(0.0, CGFloat(Double(y)))
+        
+        pipeUp.physicsBody = SKPhysicsBody(rectangleOfSize: pipeUp.size)
+        pipeUp.physicsBody?.dynamic = false
+        pipeUp.physicsBody?.categoryBitMask = pipeCategory
+        pipeUp.physicsBody?.contactTestBitMask = birdCategory
+        pipePair.addChild(pipeUp)
+        
+        var contactNode = SKNode()
+        contactNode.position = CGPointMake( pipeDown.size.width + bird.size.width / 2, CGRectGetMidY( self.frame ) )
+        contactNode.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake( pipeUp.size.width, self.frame.size.height ))
+        contactNode.physicsBody?.dynamic = false
+        contactNode.physicsBody?.categoryBitMask = scoreCategory
+        contactNode.physicsBody?.contactTestBitMask = birdCategory
+        pipePair.addChild(contactNode)
+        
+        pipePair.runAction(movePipesAndRemove)
+        pipes.addChild(pipePair)
+        
+    }
+    
+    func resetScene (){
+        // Move bird to original position and reset velocity
+        bird.position = CGPointMake(self.frame.size.width / 2.5, CGRectGetMidY(self.frame))
+        bird.physicsBody?.velocity = CGVectorMake( 0, 0 )
+        bird.physicsBody?.collisionBitMask = worldCategory | pipeCategory
+        bird.speed = 1.0
+        bird.zRotation = 0.0
+        
+        // Remove all existing pipes
+        pipes.removeAllChildren()
+        
+        // Reset _canRestart
+        canRestart = false
+        
+        // Reset score
+        score = 0
+        scoreLabelNode.text = String(score)
+        
+        // Restart animation
+        moving.speed = 1
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        if moving.speed > 0  {
+            for touch: AnyObject in touches {
+                let location = touch.locationInNode(self)
+                
+                bird.physicsBody?.velocity = CGVectorMake(0, 0)
+                bird.physicsBody?.applyImpulse(CGVectorMake(0, 10))
+                
+            }
+        }else if canRestart {
+            self.resetScene()
         }
     }
-   
+    
+    // TODO: Move to utilities somewhere. There's no reason this should be a member function
+    func clamp(min: CGFloat, max: CGFloat, value: CGFloat) -> CGFloat {
+        if( value > max ) {
+            return max
+        } else if( value < min ) {
+            return min
+        } else {
+            return value
+        }
+    }
+    
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        bird.zRotation = self.clamp( -1, max: 0.5, value: bird.physicsBody!.velocity.dy * ( bird.physicsBody!.velocity.dy < 0 ? 0.003 : 0.001 ) )
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        if moving.speed > 0 {
+            if ( contact.bodyA.categoryBitMask & scoreCategory ) == scoreCategory || ( contact.bodyB.categoryBitMask & scoreCategory ) == scoreCategory {
+                // Bird has contact with score entity
+                score++
+                scoreLabelNode.text = String(score)
+                
+                // Add a little visual feedback for the score increment
+                scoreLabelNode.runAction(SKAction.sequence([SKAction.scaleTo(1.5, duration:NSTimeInterval(0.1)), SKAction.scaleTo(1.0, duration:NSTimeInterval(0.1))]))
+            } else {
+                
+                moving.speed = 0
+                
+                bird.physicsBody?.collisionBitMask = worldCategory
+                bird.runAction(  SKAction.rotateByAngle(CGFloat(M_PI) * CGFloat(bird.position.y) * 0.01, duration:1), completion:{self.bird.speed = 0 })
+                
+                
+                // Flash background if contact is detected
+                self.removeActionForKey("flash")
+                self.runAction(SKAction.sequence([SKAction.repeatAction(SKAction.sequence([SKAction.runBlock({
+                    self.backgroundColor = SKColor(red: 1, green: 0, blue: 0, alpha: 1.0)
+                }),SKAction.waitForDuration(NSTimeInterval(0.05)), SKAction.runBlock({
+                    self.backgroundColor = self.skyColor
+                }), SKAction.waitForDuration(NSTimeInterval(0.05))]), count:4), SKAction.runBlock({
+                    self.canRestart = true
+                })]), withKey: "flash")
+            }
+        }
+    }
+    
+    func startPintxo (){
+        var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Pintxo2", ofType: ".mp3")!)
+        println(alertSound)
+        
+        // Removed deprecated use of AVAudioSessionDelegate protocol
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
+        AVAudioSession.sharedInstance().setActive(true, error: nil)
+        
+        var error:NSError?
+        audioPlayer = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+
+        
     }
 }
--- TxoroTxori/TxoroTxori/GameViewController.swift
+++ TxoroTxori/TxoroTxori/GameViewController.swift
@@ -11,25 +11,24 @@ import SpriteKit
 
 extension SKNode {
     class func unarchiveFromFile(file : NSString) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData.dataWithContentsOfFile(path, options: .DataReadingMappedIfSafe, error: nil)
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
+        
+        let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks")
+        
+        let sceneData = NSData.dataWithContentsOfFile(path!, options: .DataReadingMappedIfSafe, error: nil)
+        let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+        
+        archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+        let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
+        archiver.finishDecoding()
+        return scene
     }
 }
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
             let skView = self.view as SKView
@@ -45,11 +44,11 @@ class GameViewController: UIViewController {
             skView.presentScene(scene)
         }
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
+    
     override func supportedInterfaceOrientations() -> Int {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
             return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
@@ -57,13 +56,10 @@ class GameViewController: UIViewController {
             return Int(UIInterfaceOrientationMask.All.toRaw())
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
+    
 }
